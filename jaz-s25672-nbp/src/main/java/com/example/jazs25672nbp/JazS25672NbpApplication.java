0a454152e6582fec5c2eaa9cf0e6cb460f948c0a
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2023-06-24 15:09:07 +0200

    Add files via upload

---
+++ jaz-s25672-nbp/src/main/java/com/example/jazs25672nbp/JazS25672NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs25672nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS25672NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS25672NbpApplication.class, args);
+    }
+
+}
---
+++ jaz-s25672-nbp/src/test/java/com/example/jazs25672nbp/JazS25672NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs25672nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25672NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
